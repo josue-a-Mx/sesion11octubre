68a891c64f245f140d36d50eced9148f121232db
josue-a-Mx <josuearevalo55@gmail.com>
2019-10-11 13:42:06 -0500

    Uso de 'if' y de operadores logicos.

---
+++ js/main.js
@@ -0,0 +1,38 @@
+let cuantoAhorrar = 10000
+
+let gananciasPorDia = 500
+
+let cantidadDias = 30
+
+let ahorroTotal = cantidadDias * gananciasPorDia
+
+console.log(ahorroTotal)
+
+if (ahorroTotal >= cuantoAhorrar)
+{
+    console.log("Ya la armaste.")
+}
+else
+{
+    console.log("Sigue participando.")
+}
+
+/*----------------------------------------------------------------------------*/
+
+let fondos = 18000
+
+let precioDron = 16000
+
+let hayDeudas = false
+
+if(fondos >= precioDron && !hayDeudas)
+{
+    console.log("Date.")
+}
+else
+{
+    console.log("Ahorita no, joven.")
+}
+
+/*--------------------------------------------------------------------------*/
+
